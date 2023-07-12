d3701a9e8f8b41ff5ab31d5a087b70fcedceea19
unknown <dcstrandberg@gmail.com>
2023-07-12 08:39:04 -0500

    First (and a half) commit -- working on getting initial components stood up

---
+++ src/components/background.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import Header from './header.js';
+import ListBody from './list_body.js';
+
+const Background = (props) => {
+    // props of the form:
+    return (
+        <div className='Background'>
+            <Background 
+            />
+        </div>
+    );
+}
+
+export default Background;    
